bd2abc6791ba9fe02c54b586a58f3ff2630031cb
Gargooie <ibanezed123@gmail.com>
2019-12-22 11:43:12 +0400

    initial form

---
+++ Matrix.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+// a () b = e
+// ()  ()
+// c () d =f
+// g    h
+//
+//
+//int function(int a,int b,int c,int d, int e, int f, int g, int h);
+int function(int a,int b,int c,int d, int e, int f, int g, int h);
+
+
+int main()
+{
+
+
+
+    return 0;
+}
+
+int function(int a,int b,int c,int d, int e, int f, int g, int h){
+  e = a + b;
+  f = c + d;
+  g = a + c;
+  h = b + d;
+
+};
